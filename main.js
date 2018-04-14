ac035efcfca58f3d22134dd21cdc5e4f6f3386d5
TatyCat <williams.tatyanna@gmail.com>
2018-04-14 00:35:32 -0400

    Added tweeting feature

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var getQuote = function () {
+var getQuote = function() {
 
     let request = new XMLHttpRequest();
 
@@ -23,14 +23,33 @@ var getQuote = function () {
 
 
 var changeColor = function() {
-    document.getElementById('quoteSpace').style.color = '#' + (Math.random().toString(16) + "000000").substring(2,8);
+    var newColor = '#' + (Math.random().toString(16) + "000000").substring(2,8);
+    document.getElementById('quoteSpace').style.color = newColor;
+    // document.getElementById('quoteAuthor').style.color = newColor;
 };
-
-
-
-
 
 
 document.onload = getQuote();
 document.getElementById('moreButton').addEventListener("click", getQuote);
 document.getElementById('moreButton').addEventListener("click", changeColor);
+
+
+
+
+function tweetIt () {
+    var phrase = document.getElementById('quoteSpace').innerText + " -"+ document.getElementById('quoteAuthor').innerText;
+    var tweetUrl = 'https://twitter.com/intent/tweet?text=' +
+    // var tweetUrl = 'https://twitter.com/share?text=' +
+        encodeURIComponent(phrase)
+        + '.';
+
+    window.open(tweetUrl);
+
+}
+
+https://twitter.com/intent/tweet?text=Thank%20you.%20Come%20again..&url=http%3A%2F%2Flocalhost%3A63342%2FrandomQuoteGenerator%2Findex.html%3F_ijt%3Dkgtbt13bvab9nk40tk4q1ko4to&original_referer=http%3A%2F%2Flocalhost%3A63342%2FrandomQuoteGenerator%2Findex.html%3F_ijt%3Dkgtbt13bvab9nk40tk4q1ko4to
+
+document.getElementById('tweetQuote').addEventListener("click", tweetIt);
+
+
+
