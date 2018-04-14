20d4b22c34793d6f8c25e348386d7152b8f11b7f
TatyCat <williams.tatyanna@gmail.com>
2018-04-14 00:38:12 -0400

    Added tweeting feature

--- main.js
+++ main.js
@@ -39,15 +39,12 @@ document.getElementById('moreButton').addEventListener("click", changeColor);
 function tweetIt () {
     var phrase = document.getElementById('quoteSpace').innerText + " -"+ document.getElementById('quoteAuthor').innerText;
     var tweetUrl = 'https://twitter.com/intent/tweet?text=' +
-    // var tweetUrl = 'https://twitter.com/share?text=' +
         encodeURIComponent(phrase)
         + '.';
 
     window.open(tweetUrl);
 
 }
-
-https://twitter.com/intent/tweet?text=Thank%20you.%20Come%20again..&url=http%3A%2F%2Flocalhost%3A63342%2FrandomQuoteGenerator%2Findex.html%3F_ijt%3Dkgtbt13bvab9nk40tk4q1ko4to&original_referer=http%3A%2F%2Flocalhost%3A63342%2FrandomQuoteGenerator%2Findex.html%3F_ijt%3Dkgtbt13bvab9nk40tk4q1ko4to
 
 document.getElementById('tweetQuote').addEventListener("click", tweetIt);
 
